3646d162657b3c050b2ddab852215f2b5ef16677
Cesar Hipolito <cesar.hipolito91@gmail.com>
2020-01-24 12:09:24 -0500

    puerto y url de socket en variables

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', urlSocket: process.env.SOCKET_URL });
 });
 
 module.exports = router;
